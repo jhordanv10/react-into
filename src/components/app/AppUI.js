c0871a5195d9dffcb612341ad3cf0dd1df5206c0
jhordan villamil <jhordanvilla1000@gmail.com>
2022-02-01 14:35:21 -0500

    Nuevos componentes

---
+++ src/components/app/AppUI.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TodoContext } from '../../TodoContext';
+import { TodoHeader} from '../header';
+import { TodoCounter } from '../title';
+import { TodoSearch } from '../search';
+import { TodoList } from '../list';
+import { TodoItem } from '../items';
+import { CreateTodoButton } from '../button';
+import { Modal } from '../modal';
+import { TodoForm } from '../formModal';
+import './App.css';
+
+function AppUI() {
+
+  
+
+}
+
+export { AppUI }
