6a3d7adbde9a5941079e1b9a54ff8242fd6141e6
jhordan villamil <jhordanvilla1000@gmail.com>
2022-02-01 14:34:22 -0500

    Nuevos componentes

---
+++ src/components/app/useTodos.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useLocalStorage } from './useLocalStorage'
+
+
+
+
+function useTodos() {
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage('TODOS_VI', [])
+
+    const[openModal, setOpenModal] = React.useState(false)
+
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    const totalTodos = todos.length;
+
+    const [searchValue, setSearchValue] = React.useState('');
+
+    let searchTodos = []
+
+    if (!searchValue.length >= 1) {
+        searchTodos = todos
+    } else {
+        searchTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase()
+            const searchText = searchValue.toLowerCase()
+
+            return todoText.includes(searchText)
+        })
+    }
+
+    const addTodo = (text) => {
+        const newTodos = [...todos]
+        newTodos.push( {
+            completed: false,
+            text,
+        })
+        saveTodos(newTodos)
+    }
+
+
+    const completeTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text)
+
+        const newTodos = [...todos]
+        newTodos[todoIndex].completed = true
+        // todos[todoIndex] = {
+        //   text: todos[todoIndex].text,
+        //   completed: true,
+        // }
+
+        saveTodos(newTodos)
+    }
+
+    const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text)
+
+        const newTodos = [...todos]
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos)
+    }
+    return (
+        {
+            loading,
+            error,
+            totalTodos,
+            completedTodos,
+            searchValue,
+            setSearchValue,
+            searchTodos,
+            addTodo,
+            completeTodo,
+            deleteTodo,
+            openModal,
+            setOpenModal,
+        }
+    )
+}
+
+export { useTodos }
